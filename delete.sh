35af1a5de199f01015fbb6eada0216ddae547ac6
Nayan Jani <nayan_jani@uri.edu>
2021-04-05 10:51:00 -0400

    Nayans is trying to make the last change for C3

--- delete.sh
+++ delete.sh
@@ -3,4 +3,4 @@ this is for exam 3 and for the future
 =======
 this is for exam 3 and Jewel's Change
 >>>>>>> acda08d6ef588ca0611428c45fb6393c44573e26
-Nayans Change and then Jewel made a change
+Nayans Change and then Jewel made a change and then nayan made the last change
